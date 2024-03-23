adc9cac4ae5ae470d40d75a00dfc2fafa8cf3b05
khelechy <onyekwerekelechimac@gmail.com>
2024-03-23 17:01:53 +0100

    connection auth and event listening

--- lib/argus.rb
+++ lib/argus.rb
@@ -1,20 +1,45 @@
 
+require 'socket'
+
 class Argus 
+
   def initialize(username = "", password = "", host = "", port = 0)
     @username = username
     @password = password
     @host = host.empty? ? "127.0.0.1" : host
     @port = port == 0 ? 1337 : port
+
+    @socket = nil
   end
 
   def connect
-    puts "Staring argus client with #{@username} #{@password} #{@host} #{@port}"
+
+    @socket = TCPSocket.open(@host, @port)
+
+    send_auth_data()
+
+    loop do
+      data = @socket.readpartial(1024)
+      if !data.nil? && !data.empty?
+        puts "Received: #{data}"
+      end
+    end
+
+  rescue EOFError
+    puts "Connection closed by server"
+
+  rescue StandardError => e
+    puts "Error receiving data: #{e.message}"
+
+    puts "Socket closed"
+    @socket.close
   end
 
-  def send_auth_data
+  def send_auth_data()
     connectionString = "<ArgusAuth>#{@username}:#{@password}</ArgusAuth>"
+    @socket.puts(connectionString)
   end
 end
 
-argus = Argus.new("testusername", "tespassword")
+argus = Argus.new("testuser", "testpassword")
 argus.connect
